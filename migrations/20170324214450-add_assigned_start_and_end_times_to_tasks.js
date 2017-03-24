c2eeb34bc30a3eaa552fda50aa7d7c876b3ad4de
bernardofcs <bernardofcs@hotmail.com>
2017-03-24 22:05:20 +0000

    added more migrations

---
+++ migrations/20170324214450-add_assigned_start_and_end_times_to_tasks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return [
+      queryInterface.addColumn('tasks', 'assigned_start_time', {type: Sequelize.DATE}),
+      queryInterface.addColumn('tasks', 'assigned_end_time', {type: Sequelize.DATE})      
+      ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return [
+      queryInterface.removeColumn('tasks', 'assigned_start_time'),
+      queryInterface.removeColumn('tasks', 'assigned_end_time')      
+    ];
+  }
+};
